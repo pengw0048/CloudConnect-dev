a036403ea7e1858828c405dc0174d14287ca9c67
伪红学家 <wang-p12@mails.tsinghua.edu.cn>
2015-10-04 20:59:15 +0800

    Finish googledrive's oauth section.

--- HelloGoogledrive/HelloGoogledrive/Program.cs
+++ HelloGoogledrive/HelloGoogledrive/Program.cs
@@ -10,6 +10,22 @@ namespace HelloGoogledrive
     class Program
     {
 
+        private static string refresh_token = "1/8N6Yf_f-7KH0ms0fNH01zA4DRajTDradB6IxKMLU2RRIgOrJDtdun6zK6XiATCKT";
+        private static string client_id = "920585866822-d71k4q781qqr4rhc17jkicjdbdcn9b9d.apps.googleusercontent.com";
+        private static string client_secret = "_9rOg4xXuBQ8vDQblIcv4uZ5";
+
+        [DataContract]
+        class TokenResponse
+        {
+
+            [DataMember]
+            public long expires_in;
+
+            [DataMember]
+            public string access_token;
+
+        };
+
         static string GetResponse(HttpWebRequest req, bool GetLocation = false)
         {
             HttpWebResponse res = null;
@@ -95,8 +111,17 @@ namespace HelloGoogledrive
             return GetResponse(req);
         }
 
+        static string GetToken(string refresh_token)
+        {
+            string respHTML = HttpPost("https://www.googleapis.com/oauth2/v3/token", "client_id=" + client_id + "&client_secret=" + client_secret + "&refresh_token=" + refresh_token + "&grant_type=refresh_token", null);
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(TokenResponse));
+            TokenResponse token = (TokenResponse)ser.ReadObject(new MemoryStream(Encoding.ASCII.GetBytes(respHTML)));
+            return token.access_token;
+        }
+
         static void Main(string[] args)
         {
+            string token = GetToken(refresh_token);
 
         }
     }
