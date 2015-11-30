396ffb6e545b360fcf50053886b375d91a2ae9de
伪红学家 <wang-p12@mails.tsinghua.edu.cn>
2015-11-30 19:38:01 +0800

    Adjust project structure.

---
+++ ProtocolReceiver/ProtocolReceiver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Util = CCUtil.CCUtil;
+
+namespace ProtocolReceiver
+{
+    class ProtocolReceiver
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 1209);
+            listener.Start();
+            Console.WriteLine("Started listening.");
+
+        }
+    }
+}
--- ProtocolSender/ProtocolSender.cs
+++ ProtocolSender/ProtocolSender.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Util = CCUtil.CCUtil;
 
-namespace ProtocolServer
+namespace ProtocolSender
 {
-    class ProtocolServer
+    class ProtocolSender
     {
         static void Main(string[] args)
         {
