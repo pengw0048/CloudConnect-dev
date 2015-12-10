5535aee6176c396dcb82a931ca71f3e3514e768d
伪红学家 <wang-p12@mails.tsinghua.edu.cn>
2015-12-10 12:06:03 +0800

    Same as above.

---
+++ ProtocolSenderJ/src/protocolsenderj/FileMetadata.java
@@ -0,0 +1,20 @@
+package protocolsenderj;
+import java.io.*;
+
+public class FileMetadata implements Serializable {
+    public String name;
+    public String hash;
+    public long size;
+    public long modified;
+
+    public FileMetadata() { }
+
+    public FileMetadata(File file)
+    {
+        name = file.getName();
+        size = file.length();
+        //hash = Util.fileMD5(file.FullName);
+        modified = file.lastModified();
+    }
+}
+
--- ProtocolSenderJ/src/protocolsenderj/ProtocolSenderJ.java
+++ ProtocolSenderJ/src/protocolsenderj/ProtocolSenderJ.java
@@ -10,18 +10,55 @@ public class ProtocolSenderJ {
     public static void main(String[] args) {
         InputStream is=null;
         OutputStream os=null;
+        BufferedInputStream bis=null;
+        BufferedOutputStream bos=null;
         Socket socket=null;
-        String data="Fuck!";
         try{
+            // Establish connection to server and get stream
             socket=new Socket("127.0.0.1",1209);
             System.out.println("Connected: "+socket.toString());
             is=socket.getInputStream();
             os=socket.getOutputStream();
+            bis=new BufferedInputStream(is);
+            bos=new BufferedOutputStream(os);
             
+            // Enumerate files
+            File[] files=new File("Z:\\1").listFiles();
+            for(File file:files){
+                // Send meta
+                System.out.println(file.getName());
+                CCStream.writeStream(bos, "META");
+                FileMetadata meta = new FileMetadata(file);
+                ByteArrayOutputStream baos=new ByteArrayOutputStream();
+                ObjectOutputStream oos=new ObjectOutputStream(baos);
+                oos.writeObject(meta);
+                oos.close();
+                byte[] bytes = baos.toByteArray();
+                baos=null;
+                CCStream.writeStream(bos, bytes, true);
+                
+                // Switch server response
+                String str = CCStream.readString(bis);
+                System.out.println("--" + str);
+                if (str.equals("PASS")){
+                    // Server has the newest version
+                    System.out.println("File version up to date.");
+                }else if (str.equals("NEWF")){
+                    // This is a new file
+                    System.out.println("Sending new file.");
+                    FileInputStream fis=new FileInputStream(file.getAbsoluteFile());
+                    CCStream.copyBlockReceive(fis, bos);
+                    bos.flush();
+                    fis.close();
+                    System.out.println("File sent.");
+                }
+            }
         }catch(Exception e){
             System.out.println(e);
         }finally{
             try{
+                bos.close();
+                bis.close();
                 os.close();
                 is.close();
                 socket.close();
